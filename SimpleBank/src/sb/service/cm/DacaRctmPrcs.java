563ef64e4f56f5f9934cd6f68f64d2f42be7683d
young <young@192.168.25.8>
2017-07-19 00:14:35 +0900

    dd

--- SimpleBank/src/sb/service/cm/DacaRctmPrcs.java
+++ SimpleBank/src/sb/service/cm/DacaRctmPrcs.java
@@ -1,16 +1,16 @@
 package sb.service.cm;
 
-import org.apache.log4j.Logger;
-
 import nexcore.framework.core.data.DataSet;
 import nexcore.framework.core.data.IDataSet;
 import nexcore.framework.core.util.StringUtils;
+
+import org.apache.log4j.Logger;
+
 import sb.common.DaoHandler;
 
 public class DacaRctmPrcs {
 
 	static Logger logger = Logger.getLogger(AcnoGen.class);
-	private final String namespace = "sb.repository.mapper.DacaRctmPrcsMapper";
 	private final String rpb1000Dft = "sb.repository.mapper.RPB1000_DefaultMapper";
 	public IDataSet asDacaRctmPrcs(IDataSet requestData) throws Exception{
 		logger.debug("###########  START #########");
@@ -22,9 +22,7 @@ public class DacaRctmPrcs {
 		 *************************************************************/
 		IDataSet responseData = new DataSet();
 		DaoHandler dh = new DaoHandler();  /*DAO Handler*/
-		IDataSet dsTbl = null;
 		
-
 		try
 		{
 			/********************************************************************
@@ -34,23 +32,40 @@ public class DacaRctmPrcs {
 			
 			/*계좌잔고정보조회*/
 			
-			dsTbl = dh.selectOneSql(requestData, rpb1000Dft+"."+"S001");
+			dh.selectOneSql(requestData, rpb1000Dft+"."+"S001");
 			
 			
 			/*예수금 입금반영 호출*/
 			DacaRctmRfct drr = new DacaRctmRfct();  /*DacaRctmRfct*/
-			IDataSet drrDs = new DataSet();
+			IDataSet drrDsIn = new DataSet();
+			IDataSet drrDsOut = null; 
 			
-			drrDs.putField("TR_DT", requestData.getField("TR_DT"));
-			drrDs.putField("ACNO", requestData.getField("ACNO"));
-			drrDs.putField("RCTM_AMT", requestData.getField("RCTM_AMT"));
+			drrDsIn.putField("TR_DT", requestData.getField("TR_DT"));
+			drrDsIn.putField("ACNO", requestData.getField("ACNO"));
+			drrDsIn.putField("RCTM_AMT", requestData.getField("RCTM_AMT"));
 			
-			drr.asDacaRctmRfct(drrDs);
+			drrDsOut = drr.asDacaRctmRfct(drrDsIn);
 			
 			/*거래내역반영 호출*/
+			TrDetlRfct tdr = new TrDetlRfct();  /*거래내역반영*/
+			IDataSet tdrDsIn = new DataSet();
+			
+			tdrDsIn.putField("TR_DT", requestData.getField("TR_DT"));
+			tdrDsIn.putField("ACNO", requestData.getField("ACNO"));
+			tdrDsIn.putField("TR_NO", requestData.getLongField("TR_SN"));
+			tdrDsIn.putField("SYNS_CD", requestData.getField("SYNS_CD"));
+			tdrDsIn.putField("TR_AMT", requestData.getLongField("TR_AMT"));
+			tdrDsIn.putField("BF_DACA", drrDsOut.getLongField("BF_DACA"));
+			tdrDsIn.putField("AF_DACA", drrDsOut.getLongField("AF_DACA"));
+			tdrDsIn.putField("STRT_TR_NO", requestData.getLongField("STRT_TR_NO"));
+			tdrDsIn.putField("ORGN_TR_NO", 0);
+			tdrDsIn.putField("CLNT_NM", requestData.getField("CLNT_NM"));
+			
+			tdr.asTrDetlRfct(drrDsIn);
 			
 			/*결과값 생성*/
-			
+			responseData.putField("BF_DACA", drrDsOut.getLongField("BF_DACA"));
+			responseData.putField("AF_DACA", drrDsOut.getLongField("AF_DACA"));
 			
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -66,41 +81,38 @@ public class DacaRctmPrcs {
 	}
 	/*initCheck*/
 	private void initCheck(IDataSet requestData) throws Exception {
+	
+	
+		/*일자체크*/
+		if( StringUtils.isEmpty(requestData.getField("TR_DT"))) {
+			throw new Exception("거래일자를 확인하세요.");
+		}
 		
-		try {
-			/*일자체크*/
-			if( StringUtils.isEmpty(requestData.getField("TR_DT"))) {
-				throw new Exception("거래일자를 확인하세요.");
-			}
-			
-			/*계좌번호체크*/
-			if( StringUtils.isEmpty(requestData.getField("ACNO"))
-					|| StringUtils.length(requestData.getField("ACNO")) != 10) {
-				throw new Exception("계좌번호를 확인하세요.");
-			}
-			
-			/*거래번호*/
-			if( requestData.getLongField("TR_SN") <= 0) {
-				throw new Exception("거래번호 확인하세요.");
-			}
-			
-			/*시작거래번호*/
-			if( requestData.getLongField("STRT_TR_SN") <= 0) {
-				throw new Exception("시작거래번호 확인하세요.");
-			}
-			
-			/*총거래금액체크*/
-			if( requestData.getLongField("TOT_TR_AMT") <= 0) {
-				throw new Exception("총거래금액 확인하세요.");
-			}
-			
-			/*적요코드체크*/
-			if( StringUtils.isEmpty(requestData.getField("SYNS_CD"))
-					||StringUtils.length(requestData.getField("SYNS_CD")) != 3) {
-				throw new Exception("적요코드를 확인하세요.");
-			}
-		}catch(Exception e){
-			throw e;
+		/*계좌번호체크*/
+		if( StringUtils.isEmpty(requestData.getField("ACNO"))
+				|| StringUtils.length(requestData.getField("ACNO")) != 10) {
+			throw new Exception("계좌번호를 확인하세요.");
+		}
+		
+		/*거래번호*/
+		if( requestData.getLongField("TR_SN") <= 0) {
+			throw new Exception("거래번호 확인하세요.");
+		}
+		
+		/*시작거래번호*/
+		if( requestData.getLongField("STRT_TR_SN") <= 0) {
+			throw new Exception("시작거래번호 확인하세요.");
+		}
+		
+		/*총거래금액체크*/
+		if( requestData.getLongField("TOT_TR_AMT") <= 0) {
+			throw new Exception("총거래금액 확인하세요.");
+		}
+		
+		/*적요코드체크*/
+		if( StringUtils.isEmpty(requestData.getField("SYNS_CD"))
+				||StringUtils.length(requestData.getField("SYNS_CD")) != 3) {
+			throw new Exception("적요코드를 확인하세요.");
 		}
 		
 	}
