f4b3f2908b05b71473f1b4b8df8e34994cd069d4
young <young@192.168.25.8>
2017-09-14 01:46:56 +0900

    http://localhost:9999/SimpleBank/resources/helloworld?SERVICE_NAME=OmAcnoGen

--- SimpleBank/src/sb/common/PreServiceHandler.java
+++ SimpleBank/src/sb/common/PreServiceHandler.java
@@ -1,13 +1,22 @@
 package sb.common;
 
-import org.apache.log4j.Logger;
+import java.util.Map;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import nexcore.framework.core.data.CaseIgnoreHashMap;
 import nexcore.framework.core.data.DataSet;
 import nexcore.framework.core.data.IDataSet;
+
+import org.apache.log4j.Logger;
+
 import sb.service.om.OmAcnoDacaInqr;
 import sb.service.om.OmAcnoGen;
 import sb.service.om.OmDacaRctmPrcs;
-import javax.ws.rs.*;
 
 @Path("helloworld")
 public class PreServiceHandler {
@@ -55,9 +64,49 @@ public class PreServiceHandler {
 		return responseData;
 	}
 	
+//	 @GET
+//	 @Produces("text/plain")
+//	 /*application/json*/
+//	 public String getMessage() {
+//	      return "Rest Never Sleeps";
+//	 }
+	 
 	 @GET
-	 @Produces("text/plain")
-	 public String getMessage() {
-	      return "Rest Never Sleeps";
+	 @Produces(MediaType.APPLICATION_JSON)
+	 /*application/json text/plain  */
+	 public String getMessage(@QueryParam("SERVICE_NAME")String serviceName,@QueryParam("ACNO")String acno) {
+		/*************************************************************
+		 * Declare Var
+		 *************************************************************/
+		IDataSet responseData = new DataSet();
+		IDataSet requestData = new DataSet();
+		CaseIgnoreHashMap hsm  = null;
+		
+		DaoHandler dh = new DaoHandler();  /*DAO Handler*/
+		
+		OmAcnoGen omAcnoGen = new OmAcnoGen();
+		try 
+		{
+			 if("OmAcnoGen".equals(serviceName)) {
+					logger.debug("########### OmAcnoGen START #########");
+					requestData.putField("ACNO", acno);
+					responseData= omAcnoGen.omAcnoGen(requestData);
+					logger.debug("responseData.getFieldMap()"+responseData.getRecordSet("ResultSet"));
+					
+					
+					
+			 }
+			dh.getSession().commit();
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+			dh.getSession().rollback();
+			/*에러는 무시하고 롤백만 함.*/	
+		} finally {
+			dh.closeSession();
+		}
+		 
+	      return acno;
 	 }
 }
