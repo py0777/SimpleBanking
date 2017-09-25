53aa093fd6c3c4a243f2b12a388076d0b25a43f1
young <young@192.168.25.8>
2017-09-26 01:01:02 +0900

    df

--- SimpleBank/src/sb/service/cm/CmDacaAltnDrwgPrcs.java
+++ SimpleBank/src/sb/service/cm/CmDacaAltnDrwgPrcs.java
@@ -160,12 +160,12 @@ public class CmDacaAltnDrwgPrcs
 			responseData.putField("TR_DT", requestData.getField("TR_DT"));
 			responseData.putField("DRWG_ACNO", requestData.getField("DRWG_ACNO"));
 			responseData.putField("DRWG_TR_NO", requestData.getLongField("DRWG_TR_NO"));
-			cmTrDetlRfctDsIn.putField("DRWG_BF_DACA", cmDacaDrwgRfctDsOut.getLongField("DRWG_BF_DACA"));
-			cmTrDetlRfctDsIn.putField("DRWG_AF_DACA", cmDacaDrwgRfctDsOut.getLongField("DRWG_AF_DACA"));
+			responseData.putField("DRWG_BF_DACA", cmDacaDrwgRfctDsOut.getLongField("DRWG_BF_DACA"));
+			responseData.putField("DRWG_AF_DACA", cmDacaDrwgRfctDsOut.getLongField("DRWG_AF_DACA"));
 			responseData.putField("RCTM_ACNO", requestData.getField("RCTM_ACNO"));
 			responseData.putField("RCTM_TR_NO", requestData.getLongField("RCTM_TR_NO"));
-			rctmTrDetlRfctDsIn.putField("RCTM_BF_DACA", cmDacaRctmRfctDsOut.getLongField("BF_DACA"));
-			rctmTrDetlRfctDsIn.putField("RCTM_AF_DACA", cmDacaRctmRfctDsOut.getLongField("AF_DACA"));
+			responseData.putField("RCTM_BF_DACA", cmDacaRctmRfctDsOut.getLongField("BF_DACA"));
+			responseData.putField("RCTM_AF_DACA", cmDacaRctmRfctDsOut.getLongField("AF_DACA"));
 			
 		}catch (Exception e) {
 			e.printStackTrace();
