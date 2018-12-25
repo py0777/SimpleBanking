ced111a8e79b6e56bac38bd08d3938ec2d3fe080
Administrator <Administrator@SKCC17N00364.SKCC.NET>
2018-12-26 01:34:40 +0900

    1

--- SimpleBank/src/sb/common/PreServiceHandler.java
+++ SimpleBank/src/sb/common/PreServiceHandler.java
@@ -24,44 +24,45 @@ import sb.service.om.OmDacaRctmPrcs;
 public class PreServiceHandler {
 	
 	static Logger logger = Logger.getLogger(PreServiceHandler.class);
-	public void preServiceHandler(IDataSet requestData) throws Exception{
+	public IDataSet preServiceHandler(IDataSet requestData) throws Exception{
 		
 		
-//		/*************************************************************
-//		 * Declare Var
-//		 *************************************************************/
-//		IDataSet responseData = new DataSet();
-//		
-//		DaoHandler dh = new DaoHandler();  /*DAO Handler*/
-//		
-//		OmAcnoGen omAcnoGen = new OmAcnoGen();
-//		OmAcnoDacaInqr omAcnoDacaInqr = new OmAcnoDacaInqr();
-//		OmDacaRctmPrcs omDacaRctmPrcs = new OmDacaRctmPrcs();
-//		try 
-//		{
-//			if("OmAcnoGen".equals(requestData.getField("SERVICE"))) {
-//				logger.debug("########### OmAcnoGen START #########");
-//				responseData= omAcnoGen.omAcnoGen(requestData);
-//				
-//			}
-//			else if("OmAcnoDacaInqr".equals(requestData.getField("SERVICE"))) {
-//				responseData= omAcnoDacaInqr.omAcnoDacaInqr(requestData);
-//			}
-//			else if("OmDacaRctmPrcs".equals(requestData.getField("SERVICE"))) {
-//				responseData= omDacaRctmPrcs.omDacaRctmPrcs(requestData);
-//			}
-//			
-//			
-//			dh.getSession().commit();
-//			
-//		}catch (Exception e) {
-//			
-//			e.printStackTrace();
-//			dh.getSession().rollback();
-//			/*에러는 무시하고 롤백만 함.*/	
-//		} finally {
-//			dh.closeSession();
-//		}
+		/*************************************************************
+		 * Declare Var
+		 *************************************************************/
+		IDataSet responseData = new DataSet();
+		
+		DaoHandler dh = new DaoHandler();  /*DAO Handler*/
+		
+		OmAcnoGen omAcnoGen = new OmAcnoGen();
+		OmAcnoDacaInqr omAcnoDacaInqr = new OmAcnoDacaInqr();
+		OmDacaRctmPrcs omDacaRctmPrcs = new OmDacaRctmPrcs();
+		try 
+		{
+			if("OmAcnoGen".equals(requestData.getField("SERVICE"))) {
+				logger.debug("########### OmAcnoGen START #########");
+				responseData= omAcnoGen.omAcnoGen(requestData);
+				
+			}
+			else if("OmAcnoDacaInqr".equals(requestData.getField("SERVICE"))) {
+				responseData= omAcnoDacaInqr.omAcnoDacaInqr(requestData);
+			}
+			else if("OmDacaRctmPrcs".equals(requestData.getField("SERVICE"))) {
+				responseData= omDacaRctmPrcs.omDacaRctmPrcs(requestData);
+			}
+			
+			
+			dh.getSession().commit();
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+			dh.getSession().rollback();
+			/*에러는 무시하고 롤백만 함.*/	
+		} finally {
+			dh.closeSession();
+		}
+		return responseData;
 		
 	}
 	
